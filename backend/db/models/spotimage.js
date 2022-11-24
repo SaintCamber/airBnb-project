8752362b7a9cf02e78a609e2957aa56a5eb53880
Sarah <kretaksson@gmail.com>
2022-11-24 04:31:57 -0700

    well poop

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class SpotImage extends Model {
+  class spotimage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,20 +11,20 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      SpotImage.belongsTo(models.Spot,{foreignKey:'spotId',onDelete:"CASCADE",hooks:true})
+      spotimage.belongsTo(models.Spot,{foreignKey:'spotId',onDelete:"CASCADE",hooks:true})
     }
   }
-  SpotImage.init({
+  spotimage.init({
     spotId:{ type: DataTypes.INTEGER,references:{model:"Spots",key:"id"},onDelete:"CASCADE"},
     url: DataTypes.STRING,
     preview: {type:DataTypes.BOOLEAN,defaultValue:false}
   }, {
     sequelize,
-    modelName: 'SpotImage',
+    modelName: 'spotimage',
     scopes: {
       spotSearch: {
         attributes: { exclude: [ "createdAt", "updatedAt"] },
       },
   }});
-  return SpotImage;
+  return spotimage;
 };
