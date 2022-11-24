5b5aed8eb10d1d96d484cd313e6979e34c3a4025
Sarah <kretaksson@gmail.com>
2022-11-24 04:03:30 -0700

    well poop

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sequelize = require("sequelize");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User, Spot, SpotImage,Review,reviewImage,Booking } = require("../../db/models");
+const { User, Spot, spotimage,Review,reviewImage,Booking } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const {
@@ -47,12 +47,12 @@ router.get("/", validateQueryParameters,async (req, res, next) => {
         ],
       })
      
-        let SpotImage =await spot.getSpotImages({where:{preview:true},limit:1,attributes:{exclude:["id","createdAt","updatedAt","spotId","preview"]}})
+        let spotimage =await spot.getspotimages({where:{preview:true},limit:1,attributes:{exclude:["id","createdAt","updatedAt","spotId","preview"]}})
         //   console.log(reviews[0].toJSON().avgRating)
         spot = spot.toJSON();
         spot.avgRating = reviews[0].toJSON().avgRating;
-        if(SpotImage[0]!==undefined){
-          spot.previewImage = SpotImage[0].dataValues.url
+        if(spotimage[0]!==undefined){
+          spot.previewImage = spotimage[0].dataValues.url
 
           
         }
@@ -85,10 +85,10 @@ router.post(
   requireAuth,
   isSpotOwnedByCurrentUser,
   async (req, res, next) => {
-   newSpotImage = await SpotImage.create({spotId:req.params.spotId,url:req.body.url,preview:req.body.preview})
-    verifyNewSpotImage = await SpotImage.findByPk(newSpotImage.id)
-    if(verifyNewSpotImage){
-        res.json({id:verifyNewSpotImage.id,url:verifyNewSpotImage.url,preview:verifyNewSpotImage.preview})
+   newspotimage = await spotimage.create({spotId:req.params.spotId,url:req.body.url,preview:req.body.preview})
+    verifyNewspotimage = await spotimage.findByPk(newspotimage.id)
+    if(verifyNewspotimage){
+        res.json({id:verifyNewspotimage.id,url:verifyNewspotimage.url,preview:verifyNewspotimage.preview})
     }
 }
 
@@ -116,7 +116,7 @@ router.put("/:spotId",requireAuth,isSpotOwnedByCurrentUser,validateNewSpot,async
 router.get('/:spotId',async (req,res,next)=>{
     let spot = await Spot.findByPk(req.params.spotId)
     let owner = await spot.getOwner({attributes:{exclude:['username']}})
-    let SpotImages = await spot.getSpotImages({attributes:{exclude:["createdAt","updatedAt","spotId"]}})
+    let spotimages = await spot.getspotimages({attributes:{exclude:["createdAt","updatedAt","spotId"]}})
     let reviews = await spot.getReviews()
     let avgRating = await spot.getReviews({
         attributes: [
@@ -126,7 +126,7 @@ router.get('/:spotId',async (req,res,next)=>{
     spot.dataValues.numReviews = reviews.length
     spot.dataValues.avgStarRating = avgRating[0].dataValues.avgRating
     spot.save()      
-    let payload = {spot,SpotImages,owner}
+    let payload = {spot,spotimages,owner}
     res.json(payload)
 })
 
