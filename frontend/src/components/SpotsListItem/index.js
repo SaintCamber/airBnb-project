74f56d443719e3c78ec003d896790c001b4e9953
Sarah <kretaksson@gmail.com>
2023-01-24 08:32:45 -0800

    styling of spot tiles changed border radius"

--- frontend/src/components/SpotsListItem/index.js
+++ frontend/src/components/SpotsListItem/index.js
@@ -15,8 +15,8 @@ export default function SpotListItem({spot}){
    }
    return (
     
-    <div onClick={handleTileClick} style={{display: "flex", flexDirection: "column", backgroundColor: "white", boxSizing: "borderBox", width: "24%", height: "auto",minHeight:"380px", borderRadius: 20, alignItems: "center", margin: "0 2px",cursor:"pointer"}}>
-      {spot.previewImage ? <div style={{width:"100%",height:"60%",borderRadius:30,minHeight:"50%"}}><img src={spot.previewImage} alt="spot preview" style={{width: "100%", borderRadius: 30, objectFit: "cover", height:"100%",}}></img></div> :  <div style={{width:"100%",height:"60%",borderRadius:30}}><img src='https://www.mountaineers.org/activities/routes-places/sam-hill/@@images/a5d9a97f-f12e-4091-a372-ab551fde8a58.jpeg' alt='preview' style={{width: "100%",height:"100%", borderRadius: 30, objectFit: "cover"}} ></img></div>}
+    <div onClick={handleTileClick} style={{display: "flex", flexDirection: "column", backgroundColor: "white", boxSizing: "borderBox", width: "24%", height: "auto",minHeight:"380px",alignItems: "center", margin: "0 2px",cursor:"pointer"}}>
+      {spot.previewImage ? <div style={{width:"100%",height:"60%",minHeight:"50%"}}><img src={spot.previewImage} alt="spot preview" style={{width: "100%", borderRadius: 10, objectFit: "cover", height:"100%",}}></img></div> :  <div style={{width:"100%",height:"60%"}}><img src='https://www.mountaineers.org/activities/routes-places/sam-hill/@@images/a5d9a97f-f12e-4091-a372-ab551fde8a58.jpeg' alt='preview' style={{width: "100%",height:"100%", borderRadius: 10, objectFit: "cover"}} ></img></div>}
       <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "left"}}>
         <div style={{display: "flex", flexDirection: "row",justifyContent:"space-evenly"}}>
           <NavLink to={`/Spots/${spot.id}` }style={{width: "50%"}}>{spot.name}</NavLink> <span style={{display: "flex", flexDirection: "row", alignItems: "flex-end", width: "50%", justifyContent: "end"}}><FontAwesomeIcon icon={faStar}/>{spot.avgStarRating|0}</span>
