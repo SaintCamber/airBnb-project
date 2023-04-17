b05ab8a8b94e396eb66f2ed9ae4e0bf29d9e2d86
Sarah <kretaksson@gmail.com>
2023-04-17 10:00:17 -0700

    airbnb

--- backend/db/fakerSeeds/review.js
+++ backend/db/fakerSeeds/review.js
@@ -3,6 +3,6 @@ const faker=require('faker')
 module.exports=()=>({
     userId: Math.floor(Math.random() * 100) + 1,
     spotId: Math.floor(Math.random() * 100) + 1,
-    review: faker.lorem.paragraph(),
+    review: faker.lorem.sentence(1),
     stars: Math.floor(Math.random() * 5) + 1,
 })
