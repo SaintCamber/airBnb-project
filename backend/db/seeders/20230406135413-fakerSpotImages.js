8fdd85d23091ef9176edb10cbaaeb5d5c477f97d
Sarah <kretaksson@gmail.com>
2023-05-29 07:37:04 -0700

    seeder/model testing

--- backend/db/seeders/20230406135413-fakerSpotImages.js
+++ backend/db/seeders/20230406135413-fakerSpotImages.js
@@ -1,19 +1,19 @@
 'use strict';
 const Images = require('../fakerSeeds/createSpotImages.js');
 let options = {};
-    if(process.env.NODE_ENV === 'production'){
-      options.schema = process.env.SCHEMA;
-    }
-    options.tableName = "SpotImages";
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+options.tableName = "SpotImages";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
 
-   
-    return queryInterface.bulkInsert('SpotImages', Images, {});
+
+    return queryInterface.bulkInsert("SpotImages", Images, {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('SpotImages', {
       id: { [Op.gt]: 0 },
