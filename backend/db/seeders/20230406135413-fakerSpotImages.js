ebbdba28fb9dd212e0f2659024013d1620c0b3d1
Sarah <kretaksson@gmail.com>
2023-05-31 07:23:35 -0700

    render not functioning with seeds :/

--- backend/db/seeders/20230406135413-fakerSpotImages.js
+++ backend/db/seeders/20230406135413-fakerSpotImages.js
@@ -1,23 +1,23 @@
-// 'use strict';
-// const Images = require('../fakerSeeds/createSpotImages.js');
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;
-// }
-// options.tableName = "spotimages";
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
+'use strict';
+const Images = require('../fakerSeeds/createSpotImages.js');
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+options.tableName = "spotimages";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
 
 
-//     return queryInterface.bulkInsert("spotimages", Images, {});
-//   },
+    return queryInterface.bulkInsert("spotimages", Images, {});
+  },
 
-//   async down(queryInterface, Sequelize) {
-//     const Op = Sequelize.Op;
-//     return queryInterface.bulkDelete('spotimages', {
-//       id: { [Op.gt]: 0 },
-//     });
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('spotimages', {
+      id: { [Op.gt]: 0 },
+    });
 
-//   }
-// };
+  }
+};
